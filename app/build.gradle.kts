0e15c500accdfd160757f0cc9877d35bea7f336d
breduarm <bryan.e.armijos@gmail.com>
2024-09-21 19:44:40 -0500

    deps(room): Add room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    kotlin("kapt")
 }
 
 android {
@@ -74,4 +75,8 @@ dependencies {
     // Retrofit
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter)
+
+    // Room
+    implementation(libs.room)
+    kapt(libs.room.compiler)
 }
