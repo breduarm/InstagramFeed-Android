5a7d4f4c072a99172d3126fee095599d53355585
breduarm <bryan.e.armijos@gmail.com>
2024-09-30 11:00:00 -0500

    feat(di): Use Koin constructor DSL

--- app/src/main/java/com/beam/instragramfeed/di/AppModule.kt
+++ app/src/main/java/com/beam/instragramfeed/di/AppModule.kt
@@ -9,15 +9,17 @@ import com.beam.instragramfeed.domain.usecase.DeletePostsUseCase
 import com.beam.instragramfeed.domain.usecase.FetchPostsFromRemoteUseCase
 import com.beam.instragramfeed.domain.usecase.GetPostsUseCase
 import com.beam.instragramfeed.ui.screens.feed.FeedViewModel
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-    single<PostRemoteDataSource> { PostRetrofitDataSource(get()) }
-    single<PostLocalDataSource> { PostRoomDataSource(get()) }
-    single { PostRepository(get(), get()) }
-    single { GetPostsUseCase(get()) }
-    single { FetchPostsFromRemoteUseCase(get()) }
-    single { DeletePostsUseCase(get()) }
-    viewModel { FeedViewModel(get(), get(), get()) }
+    singleOf(::PostRetrofitDataSource) { bind<PostRemoteDataSource>() }
+    singleOf(::PostRoomDataSource) { bind<PostLocalDataSource>() }
+    singleOf(::PostRepository)
+    singleOf(::GetPostsUseCase)
+    singleOf(::FetchPostsFromRemoteUseCase)
+    singleOf(::DeletePostsUseCase)
+    viewModelOf(::FeedViewModel)
 }
