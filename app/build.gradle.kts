570d5c84274776533c962e7b325f894479da4bb2
breduarm <bryan.e.armijos@gmail.com>
2024-11-02 13:00:00 -0500

    deps: Update outdated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 
 android {
     namespace = "com.beam.instragramfeed"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.beam.instragramfeed"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
